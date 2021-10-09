7a91b8de7d18cdd9e207ef5a742818d768d700ea
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-10-09 10:06:54 +0200

    Added cheatsheet for Milan

---
+++ exercises/exercise-01/index.js
@@ -0,0 +1,102 @@
+/**
+ * TODO: Implement a function which accepts a list of numbers and returns the highest number in the list. 
+ * If the array is empty, return null.
+ * @returns {Number} Highest number
+ */
+const highestNumber = (numbers) => {
+  // TODO: Implement this function
+
+  /*
+
+  // cheatsheet
+
+  let a = 10;
+  let b = 'milan';
+  let seznam = [1,2,3,4,5];
+  let objekt = { name: 'milan', surname: 'cvijetic'};
+
+  // one way
+  for(let i = 0; i < 10; i++) {
+    // your code here
+    console.log(seznam[i]);
+  }
+
+  // or another
+  seznam.forEach(element => {
+    console.log(element);
+  })
+
+  // i'm gonna getcha getcha
+  let i = 10;
+
+  while(i < 10) {
+    console.log(seznam[i])
+    i++;  
+  }
+
+  if(neki) {
+    // potem neki tukaj
+  }
+  elseif(neki druzga) {
+    // potem neki druzga
+  }
+  else {
+    // whatever else
+  }
+  
+  */
+
+}
+
+/**
+ * TODO: Implement a function which accepts a list of numbers and returns the lowest number in the list. 
+ * If the array is empty, return null.
+ * @returns {Number} Lowest number
+ */
+const lowestNumber = (numbers) => {
+  // TODO: Implement this function
+}
+
+/**
+ * TODO: Implement a function which accepts a list of numbers and returns the number with the most decimals in the list. 
+ * If there is more than one, return the smallest one. 
+ * If the array is empty, return null.
+ * @returns {Number} Number with most decimals
+ */
+const mostDecimals = (numbers) => {
+  // TODO: Implement this function
+}
+
+/**
+ * TODO: Implement a function which accepts a list of numbers and returns the number with the least decimals in the list. 
+ * If there is more than one, return the highest one. 
+ * If the array is empty, return null.
+ * @returns {Number} Number with most decimals
+ */
+const leastDecimals = (numbers) => {
+  // TODO: Implement this function
+}
+
+const indexOfHighestNumber = () => {
+
+}
+
+const indexOfLowestNumber = () => {
+
+}
+
+const numberOfOccurencesInString = () => {
+
+}
+
+const numberOfOccurencesInStringByIndex = () => {
+
+}
+
+
+module.exports = {
+  highestNumber,
+  lowestNumber,
+  mostDecimals,
+  leastDecimals
+};
