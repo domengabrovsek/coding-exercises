be076dc2c1152855c2353e7b3c5ebdd4560182a0
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-10-09 14:17:50 +0200

    Rebased Milan branch to latest master

--- exercises/exercise-01-numbers/index.js
+++ exercises/exercise-01-numbers/index.js
@@ -8,7 +8,21 @@
  * @returns {Number} Highest number
  */
 const highestNumber = (numbers) => {
-  // TODO: Implement this function
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  let currentmax = numbers[0]
+  console.log('Max before starting loop ', currentmax)
+  console.log('Length of list ', numbers.length)
+
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(`is ${numbers[i]} bigger than ${currentmax}?`);
+    if (numbers[i] > currentmax && typeof (numbers[i]) !== 'string') {
+      currentmax = numbers[i];
+    }
+  }
+  return currentmax;
 }
 
 /**
@@ -19,7 +33,21 @@ const highestNumber = (numbers) => {
  * @returns {Number} Lowest number
  */
 const lowestNumber = (numbers) => {
-  // TODO: Implement this function
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  let currentmin = numbers[0]
+  console.log('Minimum before starting loop ', currentmin)
+  console.log('Length of list ', numbers.length)
+
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(`is ${numbers[i]} smaller than ${currentmin}?`);
+    if (numbers[i] < currentmin && typeof (numbers[i]) !== 'string') {
+      currentmin = numbers[i];
+    }
+  }
+  return currentmin;
 }
 
 /**
--- exercises/exercise-01-numbers/test.js
+++
@@ -1,7 +0,0 @@
-const { highestNumber } = require('./index');
-
-const result = highestNumber([9,14,3]);
-
-console.log('RESULT: ', result);
-
-
