3f4832be91dc9471d3f5638924f1fac1db10fe99
Milan Cvijetic <milan.cvijetic76@gmail.com>
2021-10-09 11:43:01 +0200

    implemented highest number function

--- exercises/exercise-01/index.js
+++ exercises/exercise-01/index.js
@@ -1,51 +1,24 @@
 /**
- * TODO: Implement a function which accepts a list of numbers and returns the highest number in the list. 
+ * Implement a function which accepts a list of numbers and returns the highest number in the list. 
  * If the array is empty, return null.
  * @returns {Number} Highest number
  */
 const highestNumber = (numbers) => {
-  // TODO: Implement this function
-
-  /*
-
-  // cheatsheet
-
-  let a = 10;
-  let b = 'milan';
-  let seznam = [1,2,3,4,5];
-  let objekt = { name: 'milan', surname: 'cvijetic'};
-
-  // one way
-  for(let i = 0; i < 10; i++) {
-    // your code here
-    console.log(seznam[i]);
-  }
-
-  // or another
-  seznam.forEach(element => {
-    console.log(element);
-  })
-
-  // i'm gonna getcha getcha
-  let i = 10;
-
-  while(i < 10) {
-    console.log(seznam[i])
-    i++;  
-  }
-
-  if(neki) {
-    // potem neki tukaj
-  }
-  elseif(neki druzga) {
-    // potem neki druzga
-  }
-  else {
-    // whatever else
+  if(numbers.length === 0){
+    return null;
   }
   
-  */
-
+  let currentmax = numbers[0]
+  console.log('Max before starting loop ', currentmax)
+  console.log('Length of list ', numbers.length)
+  
+  for(let i = 0; i < numbers.length; i++){
+    console.log(`is ${numbers[i]} bigger than ${currentmax}?`);
+    if(numbers[i] > currentmax && typeof(numbers[i]) !== 'string'){
+      currentmax = numbers[i];
+    }
+  }
+  return currentmax;
 }
 
 /**
