baf1b1f41f7738d9b63e49a1899ab5601aaab182
Andraz Gabrovsek <andraz.gabrovsek@gmail.com>
2021-10-20 19:00:13 +0200

    Solved linter issues

--- exercises/exercise-01-numbers/index.js
+++ exercises/exercise-01-numbers/index.js
@@ -10,24 +10,23 @@
 const highestNumber = (numbers) => {
 
   if (!numbers || numbers.length === 0) {
-    return null;    
+    return null;
   }
 
   let currentMax = numbers[0];
 
-  for(let i=0; i<numbers.length; i++){
-      
+  for (let i = 0; i < numbers.length; i++) {
 
-      if (typeof(numbers[i])==='number' && currentMax < numbers[i]) {
-        currentMax = numbers[i];
-      }
+    if (typeof (numbers[i]) === 'number' && currentMax < numbers[i]) {
+      currentMax = numbers[i];
+    }
 
-      console.log("This is current max",currentMax);
+    console.log('This is current max', currentMax);
 
   }
   return currentMax;
 
-}
+};
 
 /**
  * Function which accepts a list of numbers and returns the lowest number in the list.
@@ -38,26 +37,25 @@ const highestNumber = (numbers) => {
  */
 const lowestNumber = (numbers) => {
   // TODO: Implement this function
-  
+
   if (!numbers || numbers.length === 0) {
-    return null;    
+    return null;
   }
 
   let currentMin = numbers[0];
 
-  for(let i=0; i<numbers.length; i++){
-      
+  for (let i = 0; i < numbers.length; i++) {
 
-      if (typeof(numbers[i])==='number' && currentMin > numbers[i]) {
-        currentMin = numbers[i];
-      }
+    if (typeof (numbers[i]) === 'number' && currentMin > numbers[i]) {
+      currentMin = numbers[i];
+    }
 
-      console.log("This is current min",currentMin);
+    console.log('This is current min', currentMin);
 
   }
   return currentMin;
 
-}
+};
 
 /**
  * Function which accepts a list of numbers and returns the number with the most digits in the list.
@@ -75,17 +73,17 @@ const mostDigits = (numbers) => {
 
   let stringLengthMax = String(numbers[0]).length;
 
-  for(let i=0; i<numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
 
-    if (stringLengthMax < String(numbers[i]).length && typeof(numbers[i])==='number') {
-        stringLengthMax = String(numbers[i]).length;
+    if (stringLengthMax < String(numbers[i]).length && typeof (numbers[i]) === 'number') {
+      stringLengthMax = String(numbers[i]).length;
     }
 
   }
 
   let minLengthMax = numbers.find(number => String(number).length === stringLengthMax);
 
-  for(let i=0; i<numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     if (stringLengthMax === String(numbers[i]).length && minLengthMax > numbers[i]) {
       minLengthMax = numbers[i];
     }
@@ -93,8 +91,7 @@ const mostDigits = (numbers) => {
 
   return minLengthMax;
 
-
-}
+};
 
 /**
  * Function which accepts a list of numbers and returns the number with the least digits in the list.
@@ -106,39 +103,33 @@ const mostDigits = (numbers) => {
  */
 const leastDigits = (numbers) => {
 
-
   if (!numbers || numbers.length === 0) {
     return null;
   }
 
-  let minLength = String(numbers[0]).length; //minLength contains the length of the first element in the array
+  let minLength = String(numbers[0]).length; // minLength contains the length of the first element in the array
   let minLengthNumber = numbers[0];
-  numbers.forEach(number => { 
-    let current_num_length = String(number).length;
-    //console.log(`Number: ${number} Length: ${current_num_length}`);
+  numbers.forEach(number => {
+    const current_num_length = String(number).length;
+    // console.log(`Number: ${number} Length: ${current_num_length}`);
 
-    if (typeof(number)==="number" && minLength > current_num_length){  
+    if (typeof (number) === 'number' && minLength > current_num_length) {
       minLength = current_num_length;
       minLengthNumber = number;
     }
-  })
-console.log("This is minLenght: ",minLength);
-console.log("This is minLengthNumber:",minLengthNumber);
-
+  });
+  console.log('This is minLenght: ', minLength);
+  console.log('This is minLengthNumber:', minLengthNumber);
 
   numbers.forEach(number => {
-    if (typeof(number)==="number" && String(number).length == minLength && number > minLengthNumber){
-      minLengthNumber = number;      
+    if (typeof (number) === 'number' && String(number).length === minLength && number > minLengthNumber) {
+      minLengthNumber = number;
     }
-  })
+  });
 
   return minLengthNumber;
-  
-}
 
-
-
-
+};
 
 /**
  * Function which accepts a list of numbers and returns the sum of all numbers.
@@ -151,25 +142,19 @@ const sumNumbers = (numbers) => {
   // TODO: Implement this function
   let sumOfNumbers;
   numbers.forEach(number => {
-    if(typeof(number)==="number" && numbers.length != 0){
-      sumNumbers = number + sumOfNumbers;
+    if (typeof (number) === 'number' && numbers.length !== 0) {
+      sumOfNumbers = number + sumOfNumbers;
     }
 
-    else(numbers.length === 0) {
-      return null;  
+    else if (numbers.length === 0) {
+      return null;
     }
 
-    return sumOfNumbers;
+  }
+  );
+  return sumOfNumbers;
 
-  })
-
-}
-
-
-
-
-
-
+};
 
 /**
  * Function which accepts a list of numbers and returns the sum of odd numbers multiplied by sum of even numbers.
