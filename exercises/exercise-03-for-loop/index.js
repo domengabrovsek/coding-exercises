867630daf6d69ab699f9597d62497c1b6afe53cb
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-01-23 23:14:58 +0100

    Added for loop exercise 1

---
+++ exercises/exercise-03-for-loop/index.js
@@ -0,0 +1,13 @@
+/**
+ * Function which accepts a list of numbers and returns the sum of numbers in both arrays.
+ * @param {Array.<Number>} arr1
+ * @param {Array.<Number>} arr2
+ * @returns {Number} Sum of all elements
+ */
+const sumArrays = (arr1, arr2) => {
+  // TODO: Implement this function
+};
+
+module.exports = {
+  sumArrays
+};
